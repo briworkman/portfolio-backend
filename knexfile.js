95c130508697ac74185644bb0d8ea47c342b2d36
Brianna Workman <brianna.workman@hotmail.com>
2020-07-12 22:56:22 -0400

    adds .env  and gitignore

--- knexfile.js
+++ knexfile.js
@@ -19,4 +19,14 @@ module.exports = {
       },
     },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
