e921fb7e17c3a3fc5a992602778f57713af88ccd
Brianna Workman <brianna.workman@hotmail.com>
2020-07-14 15:23:35 -0400

    fixes cors issue

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+var cors = require('cors');
 
 const ProjectRouter = require('./projects/project-router');
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/projects', ProjectRouter);
+server.use('/api/projects', cors(), ProjectRouter);
 
 module.exports = server;
