3a18ddfa60e8db036cb647e09d2f09528636795c
Brianna Workman <brianna.workman@hotmail.com>
2020-07-14 14:25:17 -0400

    adds img source to project table

--- data/migrations/20200712220256_create-tables.js
+++ data/migrations/20200712220256_create-tables.js
@@ -1,6 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('projects', (tbl) => {
     tbl.increments();
+    tbl.string('img_src');
     tbl.string('title').notNullable();
     tbl.string('github_link').notNullable();
     tbl.string('live_link');
